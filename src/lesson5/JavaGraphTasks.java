a6a366666b335c0b1d3a827747707c644f9ba0e0
THANH-BINH <binhpttmdc@gmail.com>
2019-12-08 23:43:04 +0300

    third commit

--- src/lesson5/JavaGraphTasks.java
+++ src/lesson5/JavaGraphTasks.java
@@ -2,22 +2,24 @@ package lesson5;
 
 import kotlin.NotImplementedError;
 
-import java.util.List;
-import java.util.Set;
+import lesson5.impl.GraphBuilder;
+import org.jetbrains.annotations.Contract;
+
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaGraphTasks {
     /**
      * Эйлеров цикл.
      * Средняя
-     *
+     * <p>
      * Дан граф (получатель). Найти по нему любой Эйлеров цикл.
      * Если в графе нет Эйлеровых циклов, вернуть пустой список.
      * Соседние дуги в списке-результате должны быть инцидентны друг другу,
      * а первая дуга в списке инцидентна последней.
      * Длина списка, если он не пуст, должна быть равна количеству дуг в графе.
      * Веса дуг никак не учитываются.
-     *
+     * <p>
      * Пример:
      *
      *      G -- H
@@ -36,6 +38,7 @@ public class JavaGraphTasks {
     public static List<Graph.Edge> findEulerLoop(Graph graph) {
         throw new NotImplementedError();
     }
+
 
     /**
      * Минимальное остовное дерево.
@@ -66,7 +69,28 @@ public class JavaGraphTasks {
      * J ------------ K
      */
     public static Graph minimumSpanningTree(Graph graph) {
-        throw new NotImplementedError();
+        GraphBuilder result = new GraphBuilder();
+        HashMap<Graph.Vertex, Integer> map = new HashMap<>();
+        int i = 0;
+        for (Graph.Vertex v : graph.getVertices()) {
+            map.put(result.addVertex(v.getName()), i);
+            i++;
+        }
+        for (Graph.Edge edge : graph.getEdges()) {
+            Graph.Vertex first = edge.getBegin();
+            Graph.Vertex second = edge.getEnd();
+            if (!map.get(first).equals(map.get(second))) {
+                int oldF = map.get(first);
+                int newF = map.get(second);
+                result.addConnection(first, second, 0);
+                for (Map.Entry<Graph.Vertex, Integer> entry : map.entrySet()) {
+                    if (entry.getValue().equals(oldF)) {
+                        entry.setValue(newF);
+                    }
+                }
+            }
+        }
+        return result.build();
     }
 
     /**
@@ -82,17 +106,17 @@ public class JavaGraphTasks {
      * C -- F    I
      * |
      * E
-     *
+     * <p>
      * Найти в нём самое большое независимое множество вершин и вернуть его.
      * Никакая пара вершин в независимом множестве не должна быть связана ребром.
-     *
+     * <p>
      * Если самых больших множеств несколько, приоритет имеет то из них,
      * в котором вершины расположены раньше во множестве this.vertices (начиная с первых).
-     *
+     * <p>
      * В данном случае ответ (A, E, F, D, G, J)
-     *
+     * <p>
      * Если на входе граф с циклами, бросить IllegalArgumentException
-     *
+     * <p>
      * Эта задача может быть зачтена за пятый и шестой урок одновременно
      */
     public static Set<Graph.Vertex> largestIndependentVertexSet(Graph graph) {
@@ -109,8 +133,8 @@ public class JavaGraphTasks {
      *
      * Пример:
      *
-     *      G -- H
-     *      |    |
+     *       G -- H
+     *       |    |
      * A -- B -- C -- D
      * |    |    |    |
      * E    F -- I    |
--- src/lesson6/JavaDynamicTasks.java
+++ src/lesson6/JavaDynamicTasks.java
@@ -1,7 +1,11 @@
 package lesson6;
 
 import kotlin.NotImplementedError;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings("unused")
@@ -19,7 +23,25 @@ public class JavaDynamicTasks {
      * При сравнении подстрок, регистр символов *имеет* значение.
      */
     public static String longestCommonSubSequence(String first, String second) {
-        throw new NotImplementedError();
+        int[][] matrix = new int[first.length() + 1][second.length() + 1];
+        for (int i = 1; i <= first.length(); i++)
+            for (int j = 1; j <= second.length(); j++)
+                if (first.charAt(i - 1) == second.charAt(j - 1))
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                else matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+        StringBuilder res = new StringBuilder();
+        int i = first.length();
+        int j = second.length();
+        while (i > 0 && j > 0) {
+            if (first.charAt(i - 1) == second.charAt(j - 1)) {
+                res.append(first.charAt(i - 1));
+                i--;
+                j--;
+            } else if (matrix[i][j - 1] > matrix[i - 1][j]) {
+                j--;
+            } else i--;
+        }
+        return res.reverse().toString();
     }
 
     /**
@@ -35,7 +57,34 @@ public class JavaDynamicTasks {
      * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
      */
     public static List<Integer> longestIncreasingSubSequence(List<Integer> list) {
-        throw new NotImplementedError();
+        if (list.isEmpty()) return Collections.emptyList();
+        int size = list.size();
+        int[] prev = new int[size];
+        int[] a = new int[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = 1;
+            prev[i] = -1;
+            for (int j = 0; j < size; j++) {
+                if (list.get(j) < list.get(i) && a[j] + 1 > a[i]) {
+                    a[i] = a[j] + 1;
+                    prev[i] = j;
+                }
+            }
+        }
+        int pos = 0;
+        int length = a[0];
+        for (int i = 0; i < size; i++) {
+            if (a[i] > length) {
+                pos = i;
+                length = a[i];
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        while (pos != -1) {
+            result.add(0, list.get(pos));
+            pos = prev[pos];
+        }
+        return result;
     }
 
     /**
@@ -59,9 +108,43 @@ public class JavaDynamicTasks {
      * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
      */
     public static int shortestPathOnField(String inputName) {
-        throw new NotImplementedError();
+        List<List<Integer>> res = new ArrayList<>();
+        try (BufferedReader bf = new BufferedReader(new FileReader(inputName))) {
+            String str = bf.readLine();
+            while (str != null) {
+                List<Integer> temp = new ArrayList<>();
+                for (String ch : str.split(" ")) {
+                    temp.add(Integer.parseInt(ch));
+                }
+                res.add(temp);
+                str = bf.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int width = res.get(0).size();
+        int height = res.size();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                res.get(i).set(j, res.get(i).get(j) + min(res, i, j));
+            }
+        }
+        return res.get(height - 1).get(width - 1);
     }
-
-    // Задачу "Максимальное независимое множество вершин в графе без циклов"
-    // смотрите в уроке 5
+    private static int min(List<List<Integer>> field, int i, int j) {
+        if (i == 0 && j == 0) {
+            return 0;
+        }
+        else if (i == 0) {
+            return field.get(i).get(j - 1);
+        }
+        else if (j == 0) {
+            return field.get(i - 1).get(j);
+        }
+        else {
+            return Math.min(Math.min(field.get(i - 1).get(j - 1),
+                    field.get(i).get(j - 1)),
+                    field.get(i - 1).get(j));
+        }
+    }
 }
